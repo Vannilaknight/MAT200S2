eab60a9bbe9b97ae004efc1499abef75e5be8ec0
Neal Byington <garth033@hotmail.com>
2015-12-04 09:52:59 -0700

    Basic User Shcema outlined

--- DBShemas/UserShema.js
+++ DBShemas/UserShema.js
@@ -1,3 +1,17 @@
 /**
  * Created by Neal yingtonB on 12/4/2015.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    id: String,
+    username: String,
+    password: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    FileDirectory: String
+});
+
+module.exports = mongoose.model('User', userSchema);
