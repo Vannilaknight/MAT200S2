113951ad882c9c5da745abe83d93e2fec83d8407
Neal Byington <garth033@hotmail.com>
2015-12-04 11:01:55 -0700

    Mongo connection working with basic User Schema

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 var RSA = require('encryption');
 var num = 1435;
+var mongoose = require('mongoose');
+var dbConfig = require('./dbConfig');
+mongoose.connect(dbConfig.url);
+var User = require('./Schemas/User');
+
+//var u = new User({
+//    id: '12',
+//    firstName:"test",
+//    lastName:"tester",
+//    email:"test@test.test",
+//    username:"test",
+//    password:"pass123",
+//    FileDirectory:"null"
+//})
+//
+//u.save(function(err){
+//    if(err){
+//        console.log(err);
+//    }
+//});
+
+User.find(function(err, user){
+    if(err){console.log(err);}
+    console.log(user);
+});
 
 var RSAVars = RSA.generate();
 
