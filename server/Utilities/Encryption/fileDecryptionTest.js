38dad666abd99410ad5ccff57976efa539ff5828
Weezy <agittyturtle@gmail.com>
2015-12-07 10:35:08 -0700

    Saving to take brennens stuff and get decryption working.

--- server/Utilities/Encryption/fileDecryptionTest.js
+++ server/Utilities/Encryption/fileDecryptionTest.js
@@ -2,10 +2,20 @@
  * Created by Weezy on 12/4/2015.
  */
 
-var rsa = require('encryption');
+var rsa = require('encryption'),
+    filereader = require('filereader');
 
 exports.decryptFile = function (req, res, next) {
+    var fr = new FileReader(),
+        file = req.body.requestedFile,
+        ecrypted = '',
+        decrypted = '';
 
+        encrypted = fr.readAsBinaryString(file);
 
+        for( var i = 0; i <= encrypted.length; i += 1) {
+            var holder = rsa.decrypt(encrypted[i], d, n);
+            decrypted.push(holder);
+        }
 };
 
