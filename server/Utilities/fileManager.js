d583c64041c81ff269a8b41f023533bd876b298e
Brennen Heinrich <heinrichbrennen@gmail.com>
2015-12-07 10:33:20 -0700

    Added encryption and decryption. Neither works. Or maybe only decryption works. Idk.

--- server/Utilities/fileManager.js
+++ server/Utilities/fileManager.js
@@ -1,15 +1,29 @@
 var RSA = require('encryption'),
     mkdirp = require('mkdirp'),
     fs = require('fs'),
-    getDirName = require('path').dirname;
+    getDirName = require('path').dirname,
+    RSAVars = RSA.generate();
 
 function writeFile(path, file, callback) {
     mkdirp(getDirName(path), function(err) {
         if(err) { callback(err); }
         var fstream = fs.createWriteStream(path);
+
+        var buffer = file._readableState.buffer[0];
+
+        console.log('Decrypted: ' + buffer);
+
+        for(var i = 0; i < buffer.length; i++) {
+            buffer[i] = RSA.encrypt(buffer[i], RSAVars.n, RSAVars.e);
+        }
+
+        console.log('Encrypted: ' + buffer);
+
         file.pipe(fstream);
         fstream.on('close', function () {
             callback({success: true});
+
+            readFile('uploads/visual studio code.txt', function() {});
         });
     });
 }
@@ -17,6 +31,17 @@ function writeFile(path, file, callback) {
 function readFile(path, callback) {
     fs.readFile(path, function(err, data) {
         if(err) { callback(err); }
+
+        var buffer = data;
+
+        console.log('Encrypted: ' + buffer);
+
+        for(var i = 0; i < buffer.length; i++) {
+            buffer[i] = RSA.decrypt(buffer[i], RSAVars.d, RSAVars.n);
+        }
+
+        console.log('Decrypted: ' + buffer);
+
         callback(data);
     });
 }
