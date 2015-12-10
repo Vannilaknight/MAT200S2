6a0c1d2086f15c7e5da900008c516cb8dae7ba5e
Taylor Boyd <tboyd@student.neumont.edu>
2015-12-10 10:45:20 -0700

    initial commit of branch

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 var $ = module.exports = {};
 
 $.authorize = function(apiKey){
-
+    //LOGIN
 };
 
 $.postFile = function(file){
-    
+    //POST FILE
 };
 
 $.getFile = function(id){
-
+    //GET FILE
 };
 
 $.getTotalStorageUsed = function(){
-
+    //GET REMAINING SPACE
 };
 
 $.getStorageUsedByCust = function(String, custId){
-
+    //GET REMAINING SPACE OF CUSTOMER
 };
 
