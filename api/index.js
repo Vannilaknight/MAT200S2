3def58b8394c071e29209b5581e08144b94fe717
Taylor Boyd <tboyd@student.neumont.edu>
2015-12-10 10:36:24 -0700

    added some base files

---
+++ api/index.js
@@ -0,0 +1,22 @@
+var $ = module.exports = {};
+
+$.authorize = function(apiKey){
+
+};
+
+$.postFile = function(){
+
+};
+
+$.getFile = function(id){
+
+};
+
+$.getTotalStorageUsed = function(){
+
+};
+
+$.getStorageUsedByCust = function(String, custId){
+
+};
+
