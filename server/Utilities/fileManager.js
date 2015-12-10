819d29766e7b1f33712490536289ae852f1c1af0
Brennen Heinrich <heinrichbrennen@gmail.com>
2015-12-10 15:54:45 -0700

    Encryption works for .txt Files. Created a file builder for unencrypting files and downloading.

--- server/Utilities/fileManager.js
+++ server/Utilities/fileManager.js
@@ -2,48 +2,58 @@ var RSA = require('encryption'),
     mkdirp = require('mkdirp'),
     fs = require('fs'),
     getDirName = require('path').dirname,
-    RSAVars = RSA.generate();
+	//RSAVars = RSA.generate(); //TODO: User RSA Variables
+	RSAVars = { n: 1591, e: 191, d: 95 };
 
-function writeFile(path, file, callback) {
+function writeFile(filename, file, callback) {
+	var path = 'uploads/' + filename + '.json';
+
     mkdirp(getDirName(path), function(err) {
-        if(err) { callback(err); }
-        var fstream = fs.createWriteStream(path);
+        if(err) { callback(err); } //TODO: Error Handling
 
         var buffer = file._readableState.buffer[0];
 
-        console.log('Decrypted: ' + buffer);
+		var eFile = {
+			fileName: filename,
+			contentType: 'text/plain',
+			charset: 'UTF-8',
+			data: []
+		};
 
         for(var i = 0; i < buffer.length; i++) {
-            buffer[i] = RSA.encrypt(buffer[i], RSAVars.n, RSAVars.e);
+           eFile.data[i] = RSA.encrypt(buffer[i], RSAVars.n, RSAVars.e).value;
         }
 
-        console.log('Encrypted: ' + buffer);
+	    fs.writeFile(path, JSON.stringify(eFile), function(err) {
+		    if(err) { callback(err); } //TODO: Error Handling
 
-        file.pipe(fstream);
-        fstream.on('close', function () {
-            callback({success: true});
-
-            readFile('uploads/visual studio code.txt', function() {});
-        });
+		    callback();
+	    });
     });
 }
 
 function readFile(path, callback) {
     fs.readFile(path, function(err, data) {
-        if(err) { callback(err); }
+        if(err) { callback(err); } //TODO: Error Handling
 
-        var buffer = data;
+        var eFile = JSON.parse(data.toString());
+	    var eFileData = eFile.data;
 
-        console.log('Encrypted: ' + buffer);
+	    // Presets for Txt Files
+	    var file = {
+			contentDisposition: 'attachment; filename='+eFile.fileName,
+		    contentType: eFile.contentType,
+		    charset: eFile.charset,
+		    data: ''
+	    };
 
-        for(var i = 0; i < buffer.length; i++) {
-            buffer[i] = RSA.decrypt(buffer[i], RSAVars.d, RSAVars.n);
+        for(var i = 0; i < eFileData.length; i++) {
+            file.data += String.fromCharCode(RSA.decrypt(eFileData[i], RSAVars.d, RSAVars.n));
         }
 
-        console.log('Decrypted: ' + buffer);
-
-        callback(data);
+        callback(file);
     });
 }
 
 exports.writeFile = writeFile;
+exports.readFile = readFile;
--- server/config/routes.js
+++ server/config/routes.js
@@ -8,9 +8,21 @@ module.exports = function(app) {
     app.post('/file-upload', function(req, res, next) {
         req.pipe(req.busboy);
         req.busboy.on('file', function(fieldname, file, filename) {
-           fileManager.writeFile('uploads/' + filename, file, function() {
+           fileManager.writeFile(filename, file, function() {
                 res.redirect('/');
            });
         });
+    });
+
+    app.post('/file-download', function(req, res, next) {
+	    //var path = 'uploads/' + req.query.filename + '.json'; //TODO: Request Filename
+	    var path = 'uploads/test.txt.json';
+	    fileManager.readFile(path, function(file) {
+		    res.setHeader('Content-disposition', file.contentDisposition);
+		    res.setHeader('Content-type', file.contentType);
+		    res.charset = file.charset;
+		    res.write(file.data);
+		    res.end();
+	    });
     });
 };
