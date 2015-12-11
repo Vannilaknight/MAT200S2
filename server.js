2d383fa764d0b9107f049e2f6058dfe91c98f60d
Neal Byington <garth033@hotmail.com>
2015-12-11 10:10:41 -0700

    Added strompath to the system to create and manage users API keys.

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var mongoose = require('mongoose');
 var dbConfig = require('./dbConfig');
 mongoose.connect(dbConfig.url);
 
-var dbTests = require('./Tests/dbTests');
-dbTests.testNewUser();
+
+var stormpath_Application = require('./stormPath/Application');
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -19,7 +19,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = require('./server/config/config')[env];
 
-console.log("Original: " + num + "\nEncrypted: " + encryptedM + "\nDecrypted: " + decryptedM);
+//console.log("Original: " + num + "\nEncrypted: " + encryptedM + "\nDecrypted: " + decryptedM);
 
 require('./server/config/express')(app, config);
 require('./server/config/routes')(app);
